c247bd3c907a851eb5669387cf92baf04c122544
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-07 19:10:41 +0000

    added update and delete method for dynamic data

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://eben:olegejnr10@cluster0.celgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  },
-};
