f45b0cf7dd0afec4e18691d612b08ff1e5c5355b
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-07 20:12:13 +0000

    minor update

--- pages/dashboard/_lmao.js
+++ pages/dashboard/_lmao.js
@@ -1,10 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 const _lmao = () => {
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/notes").then(({ data }) => {
+      console.log(data);
+
+      setTitle(data.data[0].title);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>lmao</h1>
+      <h1>{title}</h1>
       {/* <a href='/'>back</a> */}
       <Link href='/'>home</Link>
     </div>
