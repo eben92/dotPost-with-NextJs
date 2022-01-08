b1d1d120be64ded1e9dabbe5d6c0d3ce8207b29e
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-08 08:39:36 +0000

    Added edit and Updated method

--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -1,13 +1,124 @@
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import fetch from "isomorphic-unfetch";
+import { Button, Form, Loader } from "semantic-ui-react";
 import { useRouter } from "next/router";
-import { Confirm, Button, Loader } from "semantic-ui-react";
-const edit = () => {
+const EditNote = ({ note }) => {
+  const [form, setForm] = useState({
+    title: note.title,
+    description: note.description,
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSubmitting) {
+      if (Object.keys(errors).length === 0) {
+        UpdateNote();
+        // alert("submited");
+        console.log("success");
+      } else {
+        setIsSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let err = validate();
+    setErrors(err);
+
+    setIsSubmitting(true);
+  };
+
+  const UpdateNote = async () => {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/notes/${router.query.id}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const validate = () => {
+    let err = {};
+
+    if (!form.title) {
+      err.title = "title is required";
+    }
+    if (!form.description) {
+      err.description = "Description is required";
+    }
+
+    return err;
+  };
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='form-container'>
+      <h1>Update Note</h1>
+      <div>
+        {isSubmitting ? (
+          <Loader active inline='centered' />
+        ) : (
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              fluid
+              error={
+                errors.title
+                  ? { content: "please enter a title", pointing: "below" }
+                  : null
+              }
+              label='title'
+              placeholder='title'
+              name='title'
+              onChange={handleChange}
+              value={form.title}
+            />
+            <Form.TextArea
+              fluid
+              error={
+                errors.title
+                  ? { content: "please enter a description", pointing: "below" }
+                  : null
+              }
+              placeholder='description'
+              label='placeholder'
+              name='description'
+              onChange={handleChange}
+              value={form.description}
+            />
+            <Button type='submit'>Update</Button>
+          </Form>
+        )}
+      </div>
     </div>
   );
 };
 
-export default edit;
+EditNote.getInitialProps = async ({ query: { id } }) => {
+  const res = await fetch(`http://localhost:3000/api/notes/${id}`);
+  const { data } = await res.json();
+
+  return { note: data };
+};
+
+export default EditNote;
