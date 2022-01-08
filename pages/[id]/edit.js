58219302d123fb1d1cca366b6c54421149d1df37
EBEN <pappy.rhutmannjnrolege@gmail.com>
2022-01-08 08:24:32 +0000

    added a delete method and functionality

--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -1,5 +1,7 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
+import { useRouter } from "next/router";
+import { Confirm, Button, Loader } from "semantic-ui-react";
 const edit = () => {
   return (
     <div>
